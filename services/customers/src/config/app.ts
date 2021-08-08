9d67e0f0a09e02d916b710af9e7b830bb154e0eb
Edvaldo Szymonek <edvaldoszy@gmail.com>
2021-08-07 22:47:56 -0300

    remove serviço não utilizado

--- services/customers/src/config/app.ts
+++
@@ -1,7 +0,0 @@
-const { PORT } = process.env;
-
-const appConfig = {
-  port: PORT,
-};
-
-export default appConfig;
--- services/customers/src/setup.ts
+++
@@ -1,3 +0,0 @@
-import { config as dotenvConfig } from '@tinyhttp/dotenv';
-
-dotenvConfig();

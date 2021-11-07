6b6affd8f38cd6957621817e2f5f40430ab14903
Edvaldo Szymonek <edvaldoszy@gmail.com>
2021-11-06 22:45:25 -0300

    Corrige dependências do projeto

--- services/todos/src/app.ts
+++ services/todos/src/app.ts
@@ -1,12 +1,12 @@
 import { logger } from '@tinyhttp/logger';
-import { json } from 'body-parser';
+import { json as parseJSON } from 'body-parser';
 
 import createApp from './factories/app';
 import routes from './routes';
 
 const app = createApp();
 app.use(logger());
-app.use(json({ strict: true }));
+app.use(parseJSON({ strict: true }));
 app.use(routes);
 
 export default app;

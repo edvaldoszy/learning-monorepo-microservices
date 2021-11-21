87deafa29e88dbc6e1f6e236baaaf45e3d81c608
Edvaldo Szymonek <edvaldoszy@gmail.com>
2021-11-20 23:54:59 -0300

    Implementa middleware de queries com criação do filtro para MongoDB

--- services/todos/src/middlewares/queries.ts
+++ services/todos/src/middlewares/queries.ts
@@ -1,8 +1,57 @@
 import { Request, Response, NextFunction } from '@tinyhttp/app';
+import { ObjectId } from 'mongodb';
 
 import appConfig from '~/config/app';
 
-async function queryMiddleware(request: Request, _: Response, next: NextFunction) {
+type WhereTuple = [string, string, any];
+type WhereObject = { andWhere: Array<WhereTuple>, orWhere: Array<WhereTuple> };
+type Filters = Array<WhereObject | WhereTuple>;
+type OperatorFunction = (value: any) => { [key: string]: any };
+
+const operatorsMap: Record<string, string | OperatorFunction> = {
+  eq: '$eq',
+  ne: '$ne',
+  gt: '$gt',
+  gte: '$gte',
+  lt: '$lt',
+  lte: '$lte',
+  in: '$in',
+  contains(value: any) {
+    const escapedValue = value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    return { $regex: new RegExp(`.*${escapedValue}.*`, 'i') };
+  },
+};
+
+function translateCondition(condition: WhereTuple) {
+  const [column, operator, value] = condition;
+  const operatorValue = operatorsMap[operator];
+  if (!operatorValue) {
+    return null;
+  }
+  if (typeof operatorValue === 'function') {
+    return {
+      [column]: operatorValue(value),
+    };
+  }
+  if (column === '_id' && ObjectId.isValid(value)) {
+    return {
+      [column]: { [operatorValue]: new ObjectId(value) },
+    };
+  }
+  const DATE_REGEX = /^\d{4}(?:-\d{2}){2}T(?:[0-1][0-9]|2[0-3])(?::[0-5][0-9]){2}(?:\.[0-9]{3})?Z$/g;
+  if (typeof value === 'string' && value.length >= 20 && value.length <= 24 && DATE_REGEX.test(value)) {
+    return {
+      [column]: { [operatorValue]: new Date(value) },
+    };
+  }
+
+  return {
+    [column]: { [operatorValue]: value },
+  };
+
+}
+
+async function queriesMiddleware(request: Request, _: Response, next: NextFunction) {
   const { query } = request;
 
   let page = parseInt(query.page as string);
@@ -26,7 +75,39 @@ async function queryMiddleware(request: Request, _: Response, next: NextFunction
     offset,
   };
 
+  let filters: Filters;
+  const { filters: filtersRequestQuery } = query;
+  if (Array.isArray(filtersRequestQuery)) {
+    filters = filtersRequestQuery.map(item => JSON.parse(item));
+  } else if (typeof filtersRequestQuery === 'string') {
+    filters = JSON.parse(filtersRequestQuery);
+  }
+
+  request.filterBy = (queryObject: object) => {
+    if (!filters) {
+      return queryObject;
+    }
+
+    const andConditions = filters
+      .map(conditionOrObject => {
+        if (Array.isArray(conditionOrObject)) {
+          return translateCondition(conditionOrObject);
+        }
+        if (conditionOrObject.orWhere) {
+          return {
+            $or: conditionOrObject.orWhere.map(translateCondition),
+          };
+        }
+        return conditionOrObject.andWhere.map(translateCondition);
+      })
+      .filter(Boolean);
+    return {
+      ...queryObject,
+      $and: andConditions,
+    };
+  };
+
   next();
 }
 
-export default queryMiddleware;
+export default queriesMiddleware;
